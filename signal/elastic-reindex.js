3481831ff1bb37b45c25a58c4bb90d6681e7f39d
rubonics <rubenrodriguez+github@gmail.com>
2022-04-06 13:43:46 -0700

    refactor signal stream interface to allow for easier reuse across the two signal types

--- signal/elastic-reindex.js
+++ signal/elastic-reindex.js
@@ -1,6 +1,6 @@
 var debug = require('debug')('signal-reindex');
-var through = require('through2');
 var signal = require( './index' )
+var streamInterface = require('./stream-interface')
 
 module.exports = SignalElasticReindex;
 module.exports.stream = SignalElasticReindexStream;
@@ -20,41 +20,21 @@ function SignalElasticReindex ( options, callback ) {
 }
 
 /**
- * Send a build signal for the site. First configure with
- * a Firebase reference, then signal with a sitename.
+ * Return stream interfaces. Seee ./stream-interface.
+ * 
+ * {
+ *   config : firebaseRef => firebaseRef,
+ *   send : {} => {},
+ * }
+ *
+ * `config` is a pass through stream that expects a firebase database
+ * reference to be passed in, and it stores a reference to it.
+ *
+ * `send` is a pass through stream that will use its stored firebase
+ * database reference to send its signal to the firebase commands node.
  */
 function SignalElasticReindexStream () {
   if ( ! ( this instanceof SignalElasticReindexStream ) ) return new SignalElasticReindexStream()
 
-  var firebase;
-
-  return {
-    config: config,
-    send  : send,
-  }
-
-  function config () {
-    return through.obj( function ( firebaseRef, enc, next ) {
-      firebase = firebaseRef;
-      next( null, firebaseRef );
-    } )
-  }
-
-  /**
-   * @param {object}   options
-   * @param {object}   options.payload
-   * @param {string}   options.payload.siteName
-   * @param {string}   options.payload.userid?
-   */
-  function send ( options ) {
-    return through.obj( function ( row, enc, next ) {
-      debug( 'send-from-stream' )
-      var signalOptions = Object.assign( options, { firebase: firebase } )
-      SignalElasticReindex( signalOptions, function ( error ) {
-        if ( error ) return next( error )
-        next( null, row )
-      } )
-    } )
-  }
-
+  return streamInterface(SignalElasticReindex)
 }
